adb6d581bf35b413a0a9df59f91346a8860d3a29
ad2701ly <shay.ryu@gmail.com>
2022-11-11 09:24:09 +0700

    Latihan IF

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,7 +4,8 @@
 	<li><a href="/2022.08.26"> 26 Agustus 2022 - Pengenalan Framework</a></li>
 	<li><a href="/2022.09.09"> 9 September 2022 - Pengenalan Javascript</a></li>
 	<li><a href="/2022.10.28"> 28 Oktober 2022 - Pengenalan Logika</a></li>
-	<li><a href="/2022.11.04"> 04 Noverber 2022 - Pengenalan Logika II</a></li>
+	<li><a href="/2022.11.04"> 04 November 2022 - Pengenalan Logika II</a></li>
+	<li><a href="/2022.11.11"> 11 November 2022 - Latihan Logika </a></li>
 </ul>
 
 <style>
---
+++ src/routes/2022.11.11/+page.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	let angka1 = 2,
+		angka2 = 0;
+</script>
+
+<halaman>
+	<div>
+		<label for="">Angka 1</label>
+		<input type="number" bind:value={angka1} />
+	</div>
+	<div>
+		<label for="">Angka 2</label>
+		<input type="number" bind:value={angka2} />
+	</div>
+
+	<p>
+		<b>{angka1}</b> dan <b>{angka2}</b> adalah angka
+		<b>{angka1 == angka2 ? 'Sama' : 'Beda'}</b>
+	</p>
+
+	<p>
+		<b>{angka1}</b> Lebih <b>{angka1 >= angka2 ? 'Besar' : 'Kecil'}</b> dari <b>{angka2}</b>
+	</p>
+</halaman>
+
+<style>
+	halaman {
+		display: flex;
+		flex-direction: column;
+		gap: 1rem;
+		padding: 10px;
+		min-height: 100%;
+	}
+</style>
